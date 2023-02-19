9e92066f6b476ea46349f8118f1074051eb5b359
Mário César Silva <mcgato33@hotmail.com>
2023-02-19 18:55:10 -0100

    first commit

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import fasttify from 'fastify'
+
+import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
+
+const app = fasttify()
+
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('HTTP Serve running!')
+  })
